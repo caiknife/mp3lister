5e073d262e3e34196a082f2768286f4e306cae6d
CarvinCai <caiknife@foxmail.com>
2024-02-29 11:12:36 +0800

    修改代码

--- cmd/db_gen/db.sql
+++ cmd/db_gen/db.sql
@@ -9,7 +9,7 @@ CREATE TABLE `songs`
     `artist`      VARCHAR(200)     NOT NULL DEFAULT '' COMMENT '艺术家',
     `album`       VARCHAR(200)     NOT NULL DEFAULT '' COMMENT '专辑',
     `bpm`         VARCHAR(5)      NOT NULL DEFAULT '' COMMENT 'BPM',
-    `origin_file` VARCHAR(200)    NOT NULL DEFAULT '' COMMENT '源文件路径',
+    `origin_file` VARCHAR(500)    NOT NULL DEFAULT '' COMMENT '源文件路径',
     PRIMARY KEY (`id`),
     KEY `idx_songs_deleted_at` (`deleted_at`)
 ) ENGINE = InnoDB
