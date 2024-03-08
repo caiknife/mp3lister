737226a4b7cd1f550282766302d569c18f091bd7
CarvinCai <caiknife@foxmail.com>
2024-03-08 10:30:20 +0800

    修改代码

--- cmd/db_gen/db.sql
+++ cmd/db_gen/db.sql
@@ -14,6 +14,24 @@ CREATE TABLE `books`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars`
+(
+    `id`           bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `created_at`   datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`   datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `deleted_at`   datetime                     DEFAULT NULL,
+    `type`         varchar(200)        NOT NULL DEFAULT '' COMMENT '车辆类型',
+    `fuel`         varchar(200)        NOT NULL DEFAULT '' COMMENT '燃料',
+    `transmission` varchar(200)        NOT NULL DEFAULT '' COMMENT '传动结构',
+    `brand`        varchar(200)        NOT NULL DEFAULT '' COMMENT '品牌',
+    `model`        varchar(200)        NOT NULL DEFAULT '' COMMENT '型号',
+    `year`         int(11)             NOT NULL DEFAULT '0' COMMENT '生产年份',
+    PRIMARY KEY (`id`),
+    KEY `deleted_at` (`deleted_at`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
 DROP TABLE IF EXISTS `movies`;
 CREATE TABLE `movies`
 (
@@ -23,10 +41,12 @@ CREATE TABLE `movies`
     `deleted_at` datetime                     DEFAULT NULL,
     `name`       varchar(200)        NOT NULL DEFAULT '' COMMENT '电影名称',
     `genre`      varchar(200)        NOT NULL DEFAULT '' COMMENT '分类',
+    `version`    int(11)                      DEFAULT NULL COMMENT '乐观锁版本号',
     PRIMARY KEY (`id`),
     KEY `name` (`name`),
     KEY `deleted_at` (`deleted_at`)
 ) ENGINE = InnoDB
+  AUTO_INCREMENT = 101
   DEFAULT CHARSET = utf8mb4;
 
 DROP TABLE IF EXISTS `songs`;
