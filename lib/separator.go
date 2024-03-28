be40243c07ca9ac046d1eb824d454b6b652887c3
CarvinCai <caiknife@hotmail.com>
2024-03-28 21:21:16 +0800

    修改代码

---
+++ lib/separator.go
@@ -0,0 +1,25 @@
+package lib
+
+import (
+	"strings"
+)
+
+const (
+	NullSeparator   = "\u0000"
+	NBSPSeparator   = "\u00A0"
+	ZWNBSPSeparator = "\uFEFF"
+)
+
+func CutInvisibleSeparator(input string) string {
+	if strings.Contains(input, NullSeparator) {
+		input = strings.ReplaceAll(input, NullSeparator, ",")
+	}
+	if strings.Contains(input, NBSPSeparator) {
+		input = strings.ReplaceAll(input, NBSPSeparator, " ")
+	}
+	if strings.Contains(input, ZWNBSPSeparator) {
+		input = strings.ReplaceAll(input, ZWNBSPSeparator, "")
+	}
+
+	return input
+}
