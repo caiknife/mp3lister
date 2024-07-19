c23e2fa22f010928fd34e9ea70d44a05f246a96b
CarvinCai <caiknife@foxmail.com>
2024-07-19 15:17:45 +0800

    redis数据筛选

--- cmd/migrate_tankcn/rediskey/funcs.go
+++ cmd/migrate_tankcn/rediskey/funcs.go
@@ -3,3 +3,19 @@ package rediskey
 func ChargeDiamondPool() string {
 	return "TK:CHARGE:DIAMOND:POOL"
 }
+
+func FirstChargePool() string {
+	return "TK:FIRSTCHARGE:POOL"
+}
+
+func HighestQualityPool() string {
+	return "TK:HIGHESTQUALITY:POOL"
+}
+
+func PlayerProficiencyExp() string {
+	return "TK:PROFICENCYEXP:PLAYER"
+}
+
+func ResetTrophyPool() string {
+	return "TK:RESETTROPHY:POOL"
+}
--- cmd/migrate_tankcn/rediskey/funcs_test.go
+++ cmd/migrate_tankcn/rediskey/funcs_test.go
@@ -1,12 +1,65 @@
 package rediskey
 
 import (
+	"context"
 	"testing"
 
 	"github.com/caiknife/mp3lister/config"
+	"github.com/caiknife/mp3lister/lib/types"
 	_ "github.com/caiknife/mp3lister/test"
 )
 
 func TestChargeDiamondPool(t *testing.T) {
-	t.Log(config.RedisDefault)
+	result, err := config.RedisDefault.HGetAll(context.TODO(), ChargeDiamondPool()).Result()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	types.Map[string](result).ForEach(func(key string, value string) {
+		t.Log(key, value, len(value))
+	})
+}
+
+func TestFirstChargePool(t *testing.T) {
+	result, err := config.RedisDefault.HGetAll(context.TODO(), FirstChargePool()).Result()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	types.Map[string](result).ForEach(func(key string, value string) {
+		t.Log(key, value, len(value))
+	})
+}
+
+func TestHighestQualityPool(t *testing.T) {
+	result, err := config.RedisDefault.HGetAll(context.TODO(), HighestQualityPool()).Result()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	types.Map[string](result).ForEach(func(key string, value string) {
+		t.Log(key, value, len(value))
+	})
+}
+
+func TestPlayerProficiencyExp(t *testing.T) {
+	result, err := config.RedisDefault.HGetAll(context.TODO(), PlayerProficiencyExp()).Result()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	types.Map[string](result).ForEach(func(key string, value string) {
+		t.Log(key, value, len(value))
+	})
+}
+
+func TestResetTrophyPool(t *testing.T) {
+	result, err := config.RedisDefault.SMembers(context.TODO(), ResetTrophyPool()).Result()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	types.Slice[string](result).ForEach(func(s string, i int) {
+		t.Log(i, s, len(s))
+	})
 }
