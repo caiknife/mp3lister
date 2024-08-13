91c185b05f2084dc13800a0d7c0a4256a918e34f
CarvinCai <caiknife@foxmail.com>
2024-08-13 11:11:20 +0800

    修改代码

--- test/snowflake_test.go
+++ test/snowflake_test.go
@@ -1,9 +1,11 @@
 package test
 
 import (
+	"net"
 	"testing"
 
 	"github.com/bwmarrin/snowflake"
+	"github.com/duke-git/lancet/v2/netutil"
 )
 
 func TestSnowflake(t *testing.T) {
@@ -19,4 +21,16 @@ func TestSnowflake(t *testing.T) {
 		id := node.Generate()
 		t.Log(id.Int64(), id.String())
 	}
+}
+
+func TestInternalIP(t *testing.T) {
+	ip := netutil.GetInternalIp()
+	t.Log(ip)
+	parsedIP := net.ParseIP(ip).To4()
+	t.Log(parsedIP[0], parsedIP[1], parsedIP[2], parsedIP[3])
+
+	b := int(parsedIP[0])<<24 + int(parsedIP[1])<<16 + int(parsedIP[2])<<8 + int(parsedIP[3])
+	t.Log(b)
+	a := int(parsedIP[2])<<8 + int(parsedIP[3])
+	t.Log(a)
 }
