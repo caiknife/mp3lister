b61a1042860f7da6a6b1860a74f2aa4db3df19f4
CarvinCai <caiknife@foxmail.com>
2024-08-19 12:19:03 +0800

    修改代码

--- cmd/migrate_tankcn/docs/db.sql
+++ cmd/migrate_tankcn/docs/db.sql
@@ -3,3 +3,21 @@ alter table wt_player
 
 alter table wt_legion
     AUTO_INCREMENT = 30000;
+
+alter table wt_gift
+    AUTO_INCREMENT = 400000;
+
+DELETE
+FROM wt_gamecenter
+WHERE player_id >= 20000000
+  AND player_id < 30000000;
+
+DELETE
+FROM wt_legion
+WHERE id >= 20000
+  AND id < 30000;
+
+DELETE
+FROM wt_player
+WHERE player_id >= 20000000
+  AND player_id < 30000000;
