4977a4470a85927b4d7dd93ce542a00a55577bef
CarvinCai <caiknife@foxmail.com>
2024-08-26 16:59:39 +0800

    修改代码

--- lib/pay/pay_test.go
+++ lib/pay/pay_test.go
@@ -1,7 +1,10 @@
 package pay
 
 import (
+	"testing"
+
 	"github.com/caiknife/mp3lister/lib"
+	"github.com/caiknife/mp3lister/lib/fjson"
 )
 
 var (
@@ -27,12 +30,22 @@ type wxpayConf struct {
 	OrderID    string `json:"order_id"`
 }
 
+func (w *wxpayConf) String() string {
+	toString, _ := fjson.MarshalToString(w)
+	return toString
+}
+
 type rustoreConf struct {
 	KeyID         string `json:"key_id"`
 	CompanyID     string `json:"company_id"`
 	PrivateKey    string `json:"private_key"`
 	PackageName   string `json:"package_name"`
 	PurchaseToken string `json:"purchase_token"`
+}
+
+func (r *rustoreConf) String() string {
+	toString, _ := fjson.MarshalToString(r)
+	return toString
 }
 
 type alipayConf struct {
@@ -42,8 +55,30 @@ type alipayConf struct {
 	OrderID    string `json:"order_id"`
 }
 
+func (a *alipayConf) String() string {
+	toString, _ := fjson.MarshalToString(a)
+	return toString
+}
+
+type quickConf struct {
+	MD5Key      string `json:"md5_key"`
+	CallbackKey string `json:"callback_key"`
+	ProductKey  string `json:"product_key"`
+	ProductCode string `json:"product_code"`
+}
+
+func (q *quickConf) String() string {
+	toString, _ := fjson.MarshalToString(q)
+	return toString
+}
+
 type pay struct {
-	Alipay  alipayConf  `json:"alipay"`
-	Wxpay   wxpayConf   `json:"wxpay"`
-	RuStore rustoreConf `json:"rustore"`
+	Alipay  *alipayConf  `json:"alipay"`
+	Wxpay   *wxpayConf   `json:"wxpay"`
+	RuStore *rustoreConf `json:"rustore"`
+	Quick   *quickConf   `json:"quick"`
+}
+
+func TestConf(t *testing.T) {
+	t.Log(Pay.Quick)
 }
