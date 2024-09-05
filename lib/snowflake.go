6ff894937f1df9b5bdae4c3f84d8294b5f95c816
CarvinCai <caiknife@foxmail.com>
2024-09-05 15:21:37 +0800

    修改代码

--- lib/snowflake.go
+++ lib/snowflake.go
@@ -8,13 +8,13 @@ import (
 	"github.com/pkg/errors"
 )
 
-const snowFlakeNodeMax = 1024
+const snowflakeNodeMax = 1024
 
 func SnowflakeMachineID() uint16 {
 	ip := netutil.GetInternalIp()
 	parseIP := net.ParseIP(ip).To4()
 	seed := uint16(parseIP[2])<<8 + uint16(parseIP[3])
-	return seed % snowFlakeNodeMax
+	return seed % snowflakeNodeMax
 }
 
 func SnowflakeNode() (*snowflake.Node, error) {
