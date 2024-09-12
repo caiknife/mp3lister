6e913f2d2ef37bc330d25b18905a2127098a0aa7
CarvinCai <caiknife@foxmail.com>
2024-09-12 10:27:45 +0800

    将tank相关的工具移到本仓库内，不再使用git submodule

--- cmd/fcm/fcm/fcm_test.go
+++ cmd/fcm/fcm/fcm_test.go
@@ -2,6 +2,7 @@ package fcm
 
 import (
 	"testing"
+	"time"
 )
 
 var (
@@ -9,6 +10,6 @@ var (
 )
 
 func TestNewFangChenMi_Sign(t *testing.T) {
-	s := mi.SignAuth(`{"data":"fUhHjbnK2ZcHNImr1UScCs1OcVEoSDpXZhi06fD2ottI44EAwxH8ycEReKTGnxjVtgulCdV6kMNSJ9754fVeU/3M2Nhb/Dv3HkuRYrdGKBGCjPVCIGnR/SZI5fpTZgEml+t+DjOzFlxADsFBBb5QRYAg0DOOchbw/zl5uKTwwePLNS4Ow2hU4D3v3nwxxxan5ypIN6GmCIid84eyEkui5Za0CKXfUXMNNuXQmLm68jFp01bIhtylk9ydaSgkFQ=="}`)
+	s := mi.SignAuth(`{"data":"fUhHjbnK2ZcHNImr1UScCs1OcVEoSDpXZhi06fD2ottI44EAwxH8ycEReKTGnxjVtgulCdV6kMNSJ9754fVeU/3M2Nhb/Dv3HkuRYrdGKBGCjPVCIGnR/SZI5fpTZgEml+t+DjOzFlxADsFBBb5QRYAg0DOOchbw/zl5uKTwwePLNS4Ow2hU4D3v3nwxxxan5ypIN6GmCIid84eyEkui5Za0CKXfUXMNNuXQmLm68jFp01bIhtylk9ydaSgkFQ=="}`, time.Now().Unix())
 	t.Log(s)
 }
